62ee4f49a428334510d82f40f4168e3817b41ea3
Ashik <lllashik2@gmail.com>
2026-09-22 10:49:37 +0530

    learn about for-loop

---
+++ for-loop.py
@@ -0,0 +1,12 @@
+fruits = ["apple","grapes","kiwi","orange"]
+# i=10
+for fruit in fruits:
+    print(fruit)
+
+# for i in range(1,100):
+#     print(i)
+
+num = 0
+while(num<=100):
+    print(num)
+    num+=5
