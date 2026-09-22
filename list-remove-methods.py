d08283dc6e463bcb731869ed34f3c6916537ee12
Ashik <lllashik2@gmail.com>
2026-09-22 10:56:56 +0530

    learn list methods

---
+++ list-remove-methods.py
@@ -0,0 +1,14 @@
+fruits = ['apple', 'grapes', 'orange', 'pineapple', 'banana']
+print(fruits)
+# remove pineapple
+fruits.remove('pineapple')
+print(fruits)
+# remove the element at the end
+fruits.pop()
+print(fruits)
+# remove the elment at index - 1
+fruits.pop(1)
+print(fruits)
+# remove all the element in the list
+fruits.clear()
+print(fruits)
