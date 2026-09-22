40fa45b273282284989c9becb919cb594242f51f
Ashik <lllashik2@gmail.com>
2026-09-22 11:03:41 +0530

    learn list methods

---
+++ list-search-methods.py
@@ -0,0 +1,5 @@
+numbers = [10, 20, 10, 30, 10]
+#return the first index of 20
+print("index of 20 = ",numbers.index(20))
+#return the count or appearance of 10
+print("count of 10  = ",numbers.count(10))
