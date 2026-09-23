4f7603fee1cd4e67c340920c3f6bc78673b81c51
Ashik <lllashik2@gmail.com>
2026-09-23 10:27:16 +0530

    learn about dictionary methods : keys , values , items

---
+++ Dictionary/dictionary_methods.py
@@ -0,0 +1,14 @@
+my_dict = {
+    "name" : "Ashik" ,
+    "age" : 21 ,
+    "place" : "vadakara"
+}
+
+#return all keys in the dictionary
+print(my_dict.keys())
+
+#return all values in the dictionary
+print(my_dict.values())
+
+# return all key-value pair in the dictionary
+print(my_dict.items())
