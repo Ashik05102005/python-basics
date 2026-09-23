f73cda267967edd2d3c13785c005d5ce74701380
Ashik <lllashik2@gmail.com>
2026-09-23 10:34:11 +0530

    learn iterating dictionry by keys , values , items

---
+++ Dictionary/dictionary_loop.py
@@ -0,0 +1,22 @@
+my_dict = {
+    "name" : "Ashik" ,
+    "age" : 21 ,
+    "place" : "vadakara"
+}
+# get all keys 
+key_list = []
+for key in my_dict : 
+    key_list.append(key)
+print(key_list)
+
+#get all values
+value_list = []
+for value in my_dict.values():
+    value_list.append(value)
+print(value_list)
+
+#get all items (key , value)
+items_list = []
+for item in my_dict.items():
+    items_list.append(item)
+print(items_list)
