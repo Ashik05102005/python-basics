171e103bc4cce19e4c782a690801c75b96fee717
Ashik <lllashik2@gmail.com>
2026-09-23 17:11:32 +0530

    learn about string search methods in , startswith endswith

---
+++ Stiring/string_search_methods.py
@@ -0,0 +1,11 @@
+
+text = "hello world"
+
+# check the substring is exist on the string 
+print("hello" in text)
+
+# check the string starts with the substring  hello 
+print(text.startswith("hello"))
+
+# check the string ends with the substring world 
+print(text.endswith("world"))
