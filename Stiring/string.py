b8f553839d62e3ab3cac8d7d6daa2b3e48bca566
Ashik <lllashik2@gmail.com>
2026-09-23 16:26:26 +0530

    learn about string , string slicing , string concatination , string repeatation

---
+++ Stiring/string.py
@@ -0,0 +1,24 @@
+
+name = "Ashik"
+greet = "Hello "
+
+# slice first three elements inddex (0-2)
+print(name[0:3])
+
+# slice first four elements index (0-3)
+print(name[:4])
+
+#slice elements from index 2 to end
+print(name[2:])
+
+# slice string in the step of 2
+print(name[::2])
+
+# reverse the string 
+print(name[::-1])
+
+# concatination
+print(greet+ " " + name)
+
+# string repeatation
+print(greet*3)
