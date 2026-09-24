73a17df313f767340c53e10d66ca5d864e54dc02
Ashik <lllashik2@gmail.com>
2026-09-24 11:04:32 +0530

    learn about keyword arguments , positional arguments , default parameters , Arbitary Arguments => *args , **kwargs

--- Functions/functions_arguments_parameters.py
+++ Functions/functions_arguments_parameters.py
@@ -16,3 +16,21 @@ def add(a=5 , b=5):
     print(a+b)
 
 add()
+
+#---Arbitary arguments---
+
+# *args -> used when we dont know the positional argument count at advance , store all afrguments as tuple  
+def add_multiple (*numbers):
+    total = 0 
+    for i in numbers:
+        total += i
+    return total
+
+res = add_multiple(2,5,8,7,6,3)
+print(res)
+
+# **kwargs -. used when we want to accept any number of keyword arguments , store all key word arguments as dictionary (key : value) 
+def student_details(**details) : 
+    print("age  = " , details['age'])
+
+student_details(name = "Ashik"  , age = 21 , course = "python ")
