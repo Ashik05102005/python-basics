5578caec4b16c21e538b388ca45a4ad79b12b0db
Ashik <lllashik2@gmail.com>
2026-09-24 13:07:33 +0530

    learn about recursion

--- Functions/Recursion.py
+++ Functions/Recursion.py
@@ -1,23 +1,26 @@
-# def countdown (n):
-#     if(n==0):
-#         return 
-#     print(n)
-#     countdown(n-1)
+def countdown (n):
+    if(n==0):
+        return 
+    print(n)
+    countdown(n-1)
+print("count down ")
+countdown(7)
 
-# countdown(7)
-
-# def factorial(n):
-#     print(n)
-#     if n==1:
-#         return 1
-#     return n*factorial(n-1)
-# print(factorial(5))
-
+print("factorial")
+def factorial(n):
+    # print(n)
+    if n==1:
+        return 1
+    return n*factorial(n-1)
+print(factorial(5))       
 
 
+print("fibinocci")
 def fibinacci(n):
     if n<=1 : 
         return n
-    print(n-1,n-2)
+    # print(n-1,n-2)
     return fibinacci(n-1)+fibinacci(n-2)
-print(fibinacci(1))
+print(fibinacci(5))
+
+
