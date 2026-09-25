b41e5f09899071af747b9c3a64a50250f2678171
Ashik <lllashik2@gmail.com>
2026-09-25 09:51:28 +0530

    learn about built-in models : math

---
+++ Models/random_test.py
@@ -0,0 +1,13 @@
+import random 
+
+# return a value that in a given range 
+print(random.randint(1,10))
+
+# return a value b/w 0-1
+print(random.random())
+
+# return a decimal number b/w the given range 
+print(random.uniform(0,20))
+
+fruits = ["mango" , "banana" , "orange" , "apple"]
+print(random.choice(fruits))
