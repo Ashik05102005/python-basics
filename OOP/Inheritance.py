de440a402d2d43fae1d863337b218f695e30bdec
Ashik <lllashik2@gmail.com>
2026-09-26 11:07:45 +0530

    learn about inheritance and types

---
+++ OOP/Inheritance.py
@@ -0,0 +1,16 @@
+
+class Animal : 
+
+    def eat(self) : 
+        print("Animal ia Eating")
+
+class Dog(Animal):
+     
+     def bark(self):
+         print("Dog is barkiing")
+
+dog = Dog()
+
+
+dog.bark()
+dog.eat()
---
+++ OOP/Inheritance_types_single.py
@@ -0,0 +1,7 @@
+class Animal :
+    def eat (self):
+        print("animal eats")
+
+class Dog (Animal) :
+    def bark (self) :
+        print("Dog barks git")
